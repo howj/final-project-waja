baf75b5d40167e41109ec0bb70a9033ec1e1eca8
howj <howj@uw.edu>
2016-12-03 16:44:52 -0800

    added plotly implementation but does not work yet

--- server.r
+++ server.r
@@ -1,25 +1,39 @@
-# install.packages("rworldmap")
 library(shiny)
 library(rworldmap)
 
-data <- read.csv("data/malaria_info.csv")
+data <- read.csv("data/overall_malaria.csv")
 
 source('./scripts/mapPolys.R')
 source('./scripts/rwmCheckAndLoadInput.R')
 source('./scripts/joinCountryData2Map.R')
 source('./scripts/getMap.R')
 source('./scripts/rwmGetISO3.R')
+source('./scripts/BuildWorldMapCases.R')
 
 sPDF <- joinCountryData2Map(data, joinCode='NAME', nameJoinColumn='Country')
+
+# light grey boundaries
+l <- list(color = toRGB("grey"), width = 0.5)
+
+# specify map projection/options
+g <- list(
+  showframe = FALSE,
+  showcoastlines = FALSE,
+  projection = list(type = 'Mercator')
+)
 
 # Generate the Map 
 shinyServer(function(input, output) {
   output$mPlot <- renderPlot({
-    mapParams <- mapPolys(sPDF, nameColumnToPlot='X2014.malaria.deaths', mapRegion='world',
+    mapParams <- mapPolys(sPDF, nameColumnToPlot=input$yearvar, mapRegion='world',
                           missingCountryCol='dark grey', numCats=10, 
                           colourPalette=c('green4','green1','greenyellow','yellow','yellow2','orange','coral','red','red3','red4'),
                           addLegend=TRUE,
                           oceanCol='light blue')
     mtext("[Grey Color: No Data Available]",side=1,line=-1)
+  })
+  
+  output$plotlyWorldMapCases <- renderPlotly({
+    return (BuildWorldMapCases(data, input$plotyear))
   })
 })
--- ui.r
+++ ui.r
@@ -6,3 +6,72 @@ mainPanel(
   )
 )
 
+shinyUI(navbarPage("Interactive Map of Malaria Deaths by Country",
+                   tabPanel("World Map and Malaria Deaths, built with rworldmap",
+                            #
+                            titlePanel("Summary of data by country"),
+                            
+                            #
+                            sidebarLayout(
+                              # User can select what data to show
+                              sidebarPanel(
+                                selectInput('yearvar', label = 'Select a year',
+                                            choices = list("2014 deaths" = "X2014.malaria.deaths",
+                                                           "2013 deaths" = "X2013.malaria.deaths", 
+                                                           "2012 deaths" = "X2012.malaria.deaths",
+                                                           "2011 deaths" = "X2011.malaria.deaths",
+                                                           "2010 deaths" = "X2010.malaria.deaths",
+                                                           "2009 deaths" = "X2009.malaria.deaths",
+                                                           "2008 deaths" = "X2008.malaria.deaths",
+                                                           "2007 deaths" = "X2007.malaria.deaths",
+                                                           "2006 deaths" = "X2006.malaria.deaths",
+                                                           "2005 deaths" = "X2005.malaria.deaths",
+                                                           "2004 deaths" = "X2004.malaria.deaths",
+                                                           "2003 deaths" = "X2003.malaria.deaths",
+                                                           "2002 deaths" = "X2002.malaria.deaths",
+                                                           "2001 deaths" = "X2001.malaria.deaths",
+                                                           "2000 deaths" = "X2000.malaria.deaths"))
+                                #sliderInput("yearvar", "Integer:", 
+                                 #           min=2000, max=2014, value=1
+                              ),
+                              
+                              # mainPanel
+                              mainPanel(
+                                plotOutput("mPlot", height="560px", width="950px")
+                              )
+                            )
+                   ),
+                   tabPanel("Interactive Map of Deaths",
+                            #
+                            titlePanel("1"),
+                            
+                            #
+                            sidebarLayout(
+                              # User can select what data to show
+                              sidebarPanel(
+                                selectInput('plotyear', label = 'Select a year',
+                                            choices = list("123" = "GDP..BILLIONS."))
+                                                           #"2014 deaths" = "X2014.malaria.deaths",
+                                                           #"2013 deaths" = "X2013.malaria.deaths", 
+                                                           #"2012 deaths" = "X2012.malaria.deaths",
+                                                           #"2011 deaths" = "X2011.malaria.deaths",
+                                                           #"2010 deaths" = "X2010.malaria.deaths",
+                                                           #"2009 deaths" = "X2009.malaria.deaths",
+                                                           #"2008 deaths" = "X2008.malaria.deaths",
+                                                           #"2007 deaths" = "X2007.malaria.deaths",
+                                                           #"2006 deaths" = "X2006.malaria.deaths",
+                                                           #"2005 deaths" = "X2005.malaria.deaths",
+                                                           #"2004 deaths" = "X2004.malaria.deaths",
+                                                           #"2003 deaths" = "X2003.malaria.deaths",
+                                                           #"2002 deaths" = "X2002.malaria.deaths",
+                                                           #"2001 deaths" = "X2001.malaria.deaths",
+                                                           #"2000 deaths" = "X2000.malaria.deaths"))
+                              ),
+                            
+                              # mainPanel
+                              mainPanel(
+                                plotOutput('map')
+                              )
+                            )
+                   )
+))
